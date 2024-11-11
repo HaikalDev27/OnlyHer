34dbb29b9f5b3233e0553f6c45f17de682a4f6a1
HaikalDev27 <haikalapriliano@gmail.com>
2024-11-12 05:15:43 +0700

    imported from local machine

---
+++ Pages/Question/YesOrNo.js
@@ -0,0 +1,25 @@
+let clickCount = 0;
+
+function moveButton() {
+    clickCount++;
+    if (clickCount < 5) {
+        const button = document.getElementById('noButton');
+        button.style.position = 'absolute';
+        button.style.top = Math.random() * 300 + 'px';
+        button.style.left = Math.random() * 300 + 'px';
+    } else {
+        alert("Yaudah deh, aku juga gabisa maksa. Maaf udah ganggu kamu.");
+        document.getElementById('formResponse').value = "No";
+        document.getElementById('submitForm').submit();
+    }
+}
+
+function submitYes() {
+    document.getElementById('formResponse').value = "Yes";
+    document.getElementById('submitForm').submit();
+}
+
+window.onload = function() {
+    var audio = document.getElementById('backgroundMusic');
+    audio.play();
+}
---
+++ main.js
@@ -0,0 +1,15 @@
+let button = document.querySelector('.button');
+let letter = document.getElementById('letter');
+let main = document.getElementById('main');
+
+button.onclick = function(){
+    main.classList.toggle("active");
+    letter.removeAttribute("hidden");
+}
+
+const envelope = document.querySelector('.envelope');
+const popup = document.querySelector('.popup');
+
+envelope.onclick = function() {
+    popup.style.display = popup.style.display === 'block' ? 'none' : 'block';
+}
